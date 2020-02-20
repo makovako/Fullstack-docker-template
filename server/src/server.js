261d94e5a54faff1e17487cf808ec2e8efff5b03
Marek Valko <valko.marek@gmail.com>
2020-02-20 16:32:54 +0100

    server biolerplate

---
+++ server/src/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const port = process.env.PORT || 4000;
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/test";
+
+const app = express();
+
+const options = {
+  useNewUrlParser: true
+};
+
+const conn = () => {
+  mongoose.connect(mongoUri, options);
+};
+
+conn();
+
+const db = mongoose.connection;
+db.on("error", err => {
+  console.log("There was a problem connecting to mongo:", err);
+  console.log("Trying again");
+  setTimeout(() => conn(), 5000);
+});
+
+db.once("open", () => console.log("Successfully connected to mongo"));
+
+app.get("/welcome", async (req, res) => {
+  console.log("Client request received");
+  const user = await User.find().exec();
+  console.log(user[0].name);
+  res.send(
+    `Hello Client! There is one record in the database for ${user[0].name}`
+  );
+});
+
+const { Schema } = mongoose;
+const userSchema = new Schema(
+  {
+    name: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+const user = new User({ name: "Big Bill Brown" });
+user
+  .save()
+  .then(user => console.log(`${user.name} saved to the database`))
+  .catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  
+})
