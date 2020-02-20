d3c1cd1706ba4125201971d541aa8f9c5622a13a
Marek Valko <valko.marek@gmail.com>
2020-02-20 18:19:06 +0100

    Mongo: Added option for index
    To surpress the deprecation warning

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,5 +1,6 @@
 export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/test";
 export const MONGO_OPTIONS = {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 }
